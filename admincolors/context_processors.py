792f51ab705ade08323f4caeec7f664f8320fff7
Victor Ferraz <victorfsf.dev@gmail.com>
2018-03-23 08:59:35 -0300

    [master] Changing how the default theme is chosen

--- admincolors/context_processors.py
+++ admincolors/context_processors.py
@@ -7,14 +7,16 @@ def admin_theme(request):
     context = {}
     if request.path.startswith(reverse('admin:index')):
         if getattr(settings, 'ADMIN_COLORS', None):
-            themes = OrderedDict([
+            theme_list = [
                 (x, y if isinstance(y, (list, tuple)) else (y, ))
                 for x, y in settings.ADMIN_COLORS
-            ])
-            context.update({
-                'themes': themes,
-                'theme': themes.get(
-                    request.COOKIES['theme']
-                ) if 'theme' in request.COOKIES else 'Default'
-            })
+            ]
+            themes = OrderedDict(theme_list)
+            if themes:
+                context.update({
+                    'themes': themes,
+                    'theme': themes.get(
+                        request.COOKIES['theme']
+                    ) if 'theme' in request.COOKIES else theme_list[0]
+                })
     return context
